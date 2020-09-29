cc218afe2d9918e55d17ab9f31424513eafece16
Anders Thor <anders.thor@sprakochfolkminnen.se>
2020-09-29 16:34:21 +0200

    Separated secret parameters into own file. (secrets.py)

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
-host = 'oden-test.sprakochfolkminnen.se:9200'
-index_name = 'isof-kartor'
-#host = 'uuc-login016-qa.its.uu.se:9200'
-#index_name = 'uu-isof-taledb-sagendatabas'
+from . import secrets
+
+print(secrets)
+host = secrets.host
+index_name = 'isof-publik'
 protocol = 'http://'
-#user = 'trada905'
-#password = 'Bqude794ed'
 cert_file = False
-restApiRecordUrl = 'http://frigg.sprakochfolkminnen.se/sagendatabas/api/records/'
-feedbackEmail = 'fredrik.skott@sprakochfolkminnen.se'
+restApiRecordUrl = 'https://frigg.isof.se/sagendatabas/api/records/'
+feedbackEmail = 'fredrik.skott@isof.se'
+LantmaterietProxy = 'http://maps.lantmateriet.se/topowebb/v1/wmts/1.0.0/topowebb/default/3006/'
+LantmaterietEpsg3857Proxy = 'http://maps.lantmateriet.se/topowebb/v1/wmts/1.0.0/topowebb/default/3857/'
+LantmaterietOrtoProxy = 'http://maps.lantmateriet.se/ortofoto/wms/v1.3/'
+LantmaterietProxy_access = secrets.LantmaterietProxy_access
+IsofGeoProxy = 'https://oden-test.isof.se/geoserver/'
+IsofHomepage = 'https://www.isof.se/'
+FriggStatic = 'https://frigg-test.isof.se/static/'
