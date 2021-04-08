091963172d1cfa7719fc62a65ae71ec7bde5c350
perlaasisof <per.laas@sprakochfolkminnen.se>
2021-04-08 17:24:36 +0200

    Lantmäteriet Topografisk webbkarta nedtonad.

--- config_prod.py
+++ config_prod.py
@@ -8,6 +8,7 @@ restApiRecordUrl = 'https://frigg.isof.se/sagendatabas/api/records/'
 feedbackEmail = 'fredrik.skott@isof.se'
 LantmaterietProxy = 'http://maps.lantmateriet.se/topowebb/v1/wmts/1.0.0/topowebb/default/3006/'
 LantmaterietEpsg3857Proxy = 'http://maps.lantmateriet.se/topowebb/v1/wmts/1.0.0/topowebb/default/3857/'
+LantmaterietNedtonadEpsg3857Proxy = 'http://maps.lantmateriet.se/topowebb/v1/wmts/1.0.0/topowebb_nedtonad/default/3857/'
 LantmaterietOrtoProxy = 'http://maps.lantmateriet.se/ortofoto/wms/v1.3/'
 LantmaterietProxy_access = secrets.LantmaterietProxy_access
 IsofGeoProxy = 'https://oden-test.isof.se/geoserver/'
